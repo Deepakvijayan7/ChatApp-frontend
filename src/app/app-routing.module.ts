dd825604abdf46b0fe693c32675c372cb0219b64
Deepakvijayan07 <deepakvijayan31@gmail.com>
2022-12-12 12:59:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { ChatsectionComponent } from './chatsection/chatsection.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'chat',component:ChatsectionComponent,canActivate:[AuthGuard],
+children:[{path:'message/:id',component:ChatboxComponent} ]
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ChatsectionComponent } from './chatsection/chatsection.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageboxComponent } from './messagebox/messagebox.component';
+import { ChatboxComponent } from './chatbox/chatbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    ChatsectionComponent,
+    HeaderComponent,
+    MessageboxComponent,
+    ChatboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
